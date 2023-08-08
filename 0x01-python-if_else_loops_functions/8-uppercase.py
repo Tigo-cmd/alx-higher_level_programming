a1429ad5316518c6f1c741a10b4f91ea514cb375
Tigo-cmd <ja16162040@gmail.com>
2023-08-08 13:12:36 -0400

    fixed logic

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-  for i in str:
-        if ord(i) >= 97 <= 122:
-            print("{}".format(chr(ord(i) - 32)), end="")
-        else:
-            print("{}".format(i), end="")
-  print()
+   for i in str:
+        print("{}".format(chr(ord(i) - 32)) if ord(i) >= 97 <= 122 else "{}".format(i), end="")
+    print()
+
