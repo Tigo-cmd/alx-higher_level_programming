4d2961edc678fef5834c0546b5f78452d849b439
Tigo-cmd <ja16162040@gmail.com>
2023-08-08 09:08:28 -0400

    fixed the logic

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+digit = 0
+if number < 0:
+    digit = (-number) % 10
+    digit = (-digit)
+else:
+    digit = number % 10
+if digit > 5:
+    print(f"Last digit of {number} is {digit} and is greater than 5")
+elif digit == 0:
+    print(f"Last digit of {number} is {digit} and is 0")
+elif digit < 6 and digit != 0:
+    print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
