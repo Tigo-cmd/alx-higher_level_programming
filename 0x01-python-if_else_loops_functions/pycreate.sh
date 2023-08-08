ffcf7bf935ab97db2b290335ecb8ff8ca0c493bd
Tigo-cmd <ja16162040@gmail.com>
2023-08-08 07:24:09 -0400

    This program will assign a random signed number to the variable number each time it is executed. Complete the source code in order to print whether the number stored in the variable number is positive or negative.

--- 0x01-python-if_else_loops_functions/pycreate.sh
+++ 0x01-python-if_else_loops_functions/pycreate.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-echo "file>"
+echo "file >"
 read file
 touch $file
 echo "#!/usr/bin/python3" > $file
-echo "Prototype>"
-read proto 
-echo $proto >> $file
 chmod 764 $file
 bash dirgit.sh
 gedit $file
