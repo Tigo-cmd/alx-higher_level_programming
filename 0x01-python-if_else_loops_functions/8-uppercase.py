b1968178c0329a3a337b8c14390e6d4d2d7816f9
Tigo-cmd <ja16162040@gmail.com>
2023-08-08 13:19:01 -0400

    still fixing pycodestyle

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-   for i in str:
-   j = ord(i)
+    for i in str:
+        j = ord(i)
         print("{}".format(chr(ord(i) - 32)) if j >= 97 <= 122 else "{}".format(i), end="")
-   print()
+    print()
