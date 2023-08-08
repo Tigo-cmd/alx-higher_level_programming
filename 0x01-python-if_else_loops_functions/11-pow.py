0c19c1bc51529e059ec87cd7bbdcab01649301d4
Tigo-cmd <ja16162040@gmail.com>
2023-08-08 17:04:35 -0400

    computed the power

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
+ if b == 0:
+        return 1
+    elif b > 0:
+        power = 1
+        for _ in range(b):
+            power = power * a
+        return power
+    else:
+        return 1 / pow(a, -b)
