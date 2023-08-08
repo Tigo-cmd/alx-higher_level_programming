522a7da2383ae6d85c84e7ac8fb7ab78761717c9
Tigo-cmd <ja16162040@gmail.com>
2023-08-08 17:18:00 -0400

    fixed indentation errors

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def pow(a, b):
- if b == 0:
+    if b == 0:
         return 1
     elif b > 0:
         power = 1
@@ -8,4 +8,4 @@ def pow(a, b):
             power = power * a
         return power
     else:
-        return 1 / pow(a, -b)
+        return 1 / pow(a, -b
