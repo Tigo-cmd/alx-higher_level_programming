9264a1afe7067db6a15ddea502d6bccff3a75db4
Tigo-cmd <ja16162040@gmail.com>
2023-08-10 15:52:15 -0400

    adds and prints the result of all arguments passed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    added = 0
+    if len(sys.argv) == 1:
+        print("{}".format(added))
+    else:
+        for i in range(1, len(sys.argv)):
+            added += int(sys.argv[i])
+        print("{}".format(added))
