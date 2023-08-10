12d1f82edd5dd056c05ec8aa2a7509b0b24a7d63
Tigo-cmd <ja16162040@gmail.com>
2023-08-10 15:39:48 -0400

    solved the algo for this dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    count = 1
+    argcount = len(sys.argv) - 1
+    if len(sys.argv) == 2:
+        print("1 argument:\n{}: {}".format(count, sys.argv[1]))
+    elif len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(argcount))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(count, sys.argv[i]))
+            count = count + 1
