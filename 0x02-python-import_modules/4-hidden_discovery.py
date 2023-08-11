161c004259021d1f1c84f7ed326c74593f5724de
Tigo-cmd <ja16162040@gmail.com>
2023-08-11 14:26:11 -0400

    decrypted it

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-open(hidden_4)
+if __name__ == "__main__":
+    import hidden_4
+    for name in names:
+        if not name.startswith("__"):
+            print("{}".format(name))
