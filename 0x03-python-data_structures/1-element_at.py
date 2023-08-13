2b59263b2e20233a10083de08d44d2cca12e48de
Tigo-cmd <ja16162040@gmail.com>
2023-08-13 15:55:57 -0400

    checks for an index from a list and retrives the value

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,2 +1,11 @@
 #\!/usr/bin/python3
 def element_at(my_list, idx):
+    if idx < 0:
+        print("none")
+        exit()
+    if idx > len(my_list):
+        print("none")
+        exit()
+    for i in range(0, len(my_list)):
+        if i == idx:
+            return my_list[i]
