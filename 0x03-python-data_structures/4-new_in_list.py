9156016def865cb27d4accc00c07655187dd039f
Tigo-cmd <ja16162040@gmail.com>
2023-08-14 06:53:12 -0400

    implemented the code

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    newlist = my_list[:]
+    if idx < 0:
+        return newlist
+    if idx > len(my_list):
+        return newlist
+    for i in range(0, len(my_list)):
+        if i == idx:
+            newlist[i] = element
+    return newlist
