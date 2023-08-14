542584711da1c0d32105a81059b96fc41307e87a
Tigo-cmd <ja16162040@gmail.com>
2023-08-14 06:26:05 -0400

    except value error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    for i in my_list[::-1]:
+        try:
+            print("{:d}".format(i))
+        except ValueError:
+            continue
