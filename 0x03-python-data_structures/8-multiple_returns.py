811f009d1121de94525f4015f35733cf98b61477
Tigo-cmd <ja16162040@gmail.com>
2023-08-14 16:13:26 -0400

    eturns a tuple with the length of a string and its first character.

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    new_list = []
+    if sentence == "":
+        length = 0
+        sentence = None
+        new_list.append(length)
+        new_list.append(sentence)
+    else:
+        length = len(sentence)
+        i = sentence[:1]
+        new_list.append(length)
+        new_list.append(i)
+    return tuple(new_list)
