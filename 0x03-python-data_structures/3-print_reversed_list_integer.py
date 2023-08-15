f0d675f9e16930d58e1c5b1d1fe19e3823b51a62
Tigo-cmd <ja16162040@gmail.com>
2023-08-15 05:25:01 -0400

    fixed bug in file 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return my_list
     for i in my_list[::-1]:
-        try:
-            print("{:d}".format(i))
-        except ValueError:
-            continue
+        print("{:d}".format(i))
