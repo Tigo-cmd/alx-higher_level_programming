9384ff46de0f60eb77f1c42a92e7d1bd1aed8d8a
Tigo-cmd <ja16162040@gmail.com>
2023-08-15 05:18:31 -0400

    fixing bugs

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        print("none")
+        return None
         exit()
     if idx > len(my_list):
-        print("none")
+        return None
         exit()
     for i in range(0, len(my_list)):
         if i == idx:
