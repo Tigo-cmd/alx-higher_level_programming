e39978655f70d484edecfa9e19799aeae4bb97ee
Tigo-cmd <ja16162040@gmail.com>
2023-08-15 05:14:25 -0400

    cross checking files

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in my_list:
-        try:
-            print("{:d}".format(i))
-        except ValueError:
-            continue
+        print("{:d}".format(i))
+
