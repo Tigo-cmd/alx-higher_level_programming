bc5f6671252fe345aa5d299ac466cc0a28e30064
Tigo-cmd <ja16162040@gmail.com>
2023-08-17 07:54:18 -0400

    replaces all occurrences of an element by another in a new list.

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in range(0, len(matrix)):
+        res = list(map(lambda x: x*x, matrix[i]))
+        new_matrix.append(res)
+    return new_matrix
