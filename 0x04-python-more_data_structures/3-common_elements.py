f61ec43de7843b01dccaab089530ea4b5638683c
Tigo-cmd <ja16162040@gmail.com>
2023-08-17 08:55:33 -0400

    working with list in python pays off

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    res = ""
+    res = []
     for i in set_1:
         for j in set_2:
             if i == j:
-                res += j
+                res.append(j)
     return res
