fabdefaf7fbae566b7fc6b3a05cb9d91cbee8963
Tigo-cmd <ja16162040@gmail.com>
2023-08-17 08:33:22 -0400

    filters and adds

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    added = 0
+    new_list = []
+    for i in my_list:
+        if i not in new_list:
+            new_list.append(i)
+    for j in new_list:
+        added += j
+    return added
