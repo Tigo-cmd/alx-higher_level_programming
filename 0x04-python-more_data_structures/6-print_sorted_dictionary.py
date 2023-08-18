131f9737e759d5585d96dc28797157e4f17c8743
Tigo-cmd <ja16162040@gmail.com>
2023-08-18 13:26:56 -0400

    prints a ordered dictionary keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    key_list = []
+    for i in sorted(a_dictionary):
+        key_list.append(i)
+    for j in key_list:
+        print("{}: {}".format(j, a_dictionary.get(j)))
