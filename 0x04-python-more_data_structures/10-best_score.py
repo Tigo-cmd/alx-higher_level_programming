e78e8bc552bcf803374efd82a48207818d8810ee
Tigo-cmd <ja16162040@gmail.com>
2023-08-18 17:48:15 -0400

    returns a key with the biggest integer value.

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    max_item = next(iter(a_dictionary))
+    for i in a_dictionary:
+        if a_dictionary.get(i) > a_dictionary.get(max_item):
+            max_item = i
+    return max_item
