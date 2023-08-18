bc8757a9a8e0dcc53ebb2a4555dd7f6283f60707
Tigo-cmd <ja16162040@gmail.com>
2023-08-18 19:30:31 -0400

    returns the weighted average of all integers tuple (<score>, <weight>)

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    run_sum = 0
+    nd_elem = 0
+    for i in my_list:
+        for j in i:
+            run_sum += i[-1] * i[-2]
+            nd_elem += i[-1]
+    return run_sum / nd_elem
