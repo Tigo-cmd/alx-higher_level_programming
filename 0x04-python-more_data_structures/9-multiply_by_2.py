73324d5c378e04104ca1a8782e8aa7025dbf1128
Tigo-cmd <ja16162040@gmail.com>
2023-08-18 17:24:30 -0400

    it works just check the logs

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    for i in a_dictionary:
+        mul = a_dictionary.get(i) * 2
+        new_dict[i] = mul
+    return new_dict
