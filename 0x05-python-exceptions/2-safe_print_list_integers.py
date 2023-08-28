9b35779c665efb934a1f8722f5098030378a9eb7
Tigo-cmd <ja16162040@gmail.com>
2023-08-28 14:10:25 -0400

    it takes patience to get whatever you want

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end="")
+            else:
+                j -= 1
+            j += 1
+        print()
+        return j
+    except TypeError:
+        pass
