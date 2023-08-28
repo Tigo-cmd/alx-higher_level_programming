bb26f45a53188fd698f58c179b11e6f4adec086e
Tigo-cmd <ja16162040@gmail.com>
2023-08-28 04:04:06 -0400

    function that prints x elements of a list.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
+    try:
+        for j in my_list:
+            if i == x:
+                break
+            else:
+                print(j, end="")
+            i += 1
+        print()
+        return i
+    except TypeError:
+        pass
