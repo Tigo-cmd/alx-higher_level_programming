9c23359b3ba4b08a62d6edd2ce2c64584cbbc67f
Tigo-cmd <ja16162040@gmail.com>
2023-08-29 06:39:46 -0400

    debugging error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -17,4 +17,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             new_list.append(0)
     finally:
-        return new_list
+        pass
+    return new_list
