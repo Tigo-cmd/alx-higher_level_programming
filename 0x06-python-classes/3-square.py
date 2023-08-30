71fd3dde301f52978b65f1ab65951ef32e580126
Tigo-cmd <ja16162040@gmail.com>
2023-08-30 18:26:13 -0400

    class Square that defines a square by: (based on 2-square.py)

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/python3#!/usr/bin/python3
+"""This module defines a class"""
+
+
+class Square:
+    """An empty class that defines a square
+    __init__: initializes the constructor
+    """
+
+    def __init__(self, size=0):
+        """Defines an attribute and initializes the size attribute
+        size: size of the square many things depend on it (area computation, etc.)
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Public attribute that returns the current square area"""
+        return self.__size ** 2
