b46cff122fd30dd9a793fdf0b8f3d3c41571bb46
Tigo-cmd <ja16162040@gmail.com>
2023-08-30 19:59:59 -0400

    class Square that defines a square by: (based on 4-square.py) pridded

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+"""This module defines a class"""
+
+
+class Square:
+    """An empty class that defines a square
+    __init__: initializes the constructor
+    """
+
+    def __init__(self, size=0):
+        """Defines an attribute and initializes the size attribute
+        size: size of the square many things depend on it (area computation, etc.)
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter function for size this sets the size variable"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter function for Size which initializes the size to value"""
+        if not isinstance(value, int):
+            raise TypeError("Size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Public attribute that returns the current square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """this function prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                print("#" * self.__size)
