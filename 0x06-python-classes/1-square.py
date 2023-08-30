fb3608e2ff6721c4c4e5002f4aa80036e1b2a299
Tigo-cmd <ja16162040@gmail.com>
2023-08-30 16:30:57 -0400

    class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""This module defines a class"""
+
+
+class Square:
+    """An empty class that defines a square
+    __init__: initializes the constructor
+    """
+    def __init__(self, size):
+        """Defines an attribute and initializes the size attribute
+        size: size of the square many things depend on it (area computation, etc.)
+        """
+        self.__size = size
