ebef0097430fe911568785791e721eb2b53dbc2a
Tigo-cmd <ja16162040@gmail.com>
2023-09-05 05:18:38 -0400

    debugging

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -4,8 +4,3 @@
 
 class Rectangle:
     """This class defines a Rectangle and it's properties"""
-    
-    def __init__(self, sides=0):
-        """initial constructor function for the rectangle class"""
-        
-        self.sides = sides
