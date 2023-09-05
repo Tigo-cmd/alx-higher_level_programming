cf31008f8e1d20368be5919e987f920948a6a3e6
Tigo-cmd <ja16162040@gmail.com>
2023-09-05 05:39:09 -0400

    class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,1 +1,46 @@
 #!/usr/bin/python3
+"""This module defines rectangle class"""
+
+
+class Rectangle:
+    """This class defines a Rectangle and it's properties
+       Args:
+            __width: private attribute
+            __height: private property for the rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        """initial constructor function for the rectangle class"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Getter property of the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, param):
+        """setter property for the width which checks"""
+        if not isinstance(param, int):
+            raise TypeError("width must be an integer")
+        elif param < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = param
+
+    @property
+    def height(self):
+        """Getter property of the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, param):
+        """setter property for the height which checks"""
+        if not isinstance(param, int):
+            raise TypeError("height must be an integer")
+        elif param < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = param
+
