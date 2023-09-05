60879170356ea516303d08e0d857e913b906891e
Tigo-cmd <ja16162040@gmail.com>
2023-09-05 05:12:12 -0400

    an empty class Rectangle that defines a rectangle:

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""This module defines an empty rectangle class"""
+
+
+class Rectangle:
+    """This class defines a Rectangle and it's properties"""
+    def __init__(self, sides=0):
+        """initial constructor function for the rectangle class"""
+        self.sides = sides
