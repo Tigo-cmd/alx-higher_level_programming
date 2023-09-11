f5f324dc5694207634b0bc4fc7ee82b4e807191c
Tigo-cmd <ja16162040@gmail.com>
2023-09-11 15:45:52 -0400

    added documentations

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""this module the list of available attributes and methods of an object"""
+
+
 def lookup(obj):
+	"""this module the list of available attributes and methods of an object"""
+	
     return dir(obj)
