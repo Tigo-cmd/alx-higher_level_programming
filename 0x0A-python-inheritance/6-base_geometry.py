5b77b85723c5cc952df5ea9b4ed3b15037f51981
Tigo-cmd <ja16162040@gmail.com>
2023-09-11 17:52:00 -0400

    class BaseGeometry (based on 5-base_geometry.py).

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""class BaseGeometry."""
+
+
+class BaseGeometry:
+    """implements the base geometry"""
+    def area(self):
+    """implementes the area of the base_geometry raise an exception if not implemented"""
+        raise Exception("area() is not implemented")
