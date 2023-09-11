64673f36de6f904baffb7bf8ee824370c0ed6368
Tigo-cmd <ja16162040@gmail.com>
2023-09-11 16:29:54 -0400

    a class MyList that inherits from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" a class MyList that inherits from list"""
+
+
+class MyList(list):
+    """returns the list in sorted order"""
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
