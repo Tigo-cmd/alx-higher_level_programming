ad7fbb585897b28cf083840be3a877a4c264feb7
Tigo-cmd <ja16162040@gmail.com>
2023-09-11 18:20:59 -0400

    class BaseGeometry (based on 6-base_geometry.py).

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""class BaseGeometry."""
+
+
+class BaseGeometry:
+    """implements the base geometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates integers and raises Exception if failed """
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater then 0")
