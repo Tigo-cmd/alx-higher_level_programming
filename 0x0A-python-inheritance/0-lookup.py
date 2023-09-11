4f0abe862d6ab0429f0220fbd36c8d161a586a97
Tigo-cmd <ja16162040@gmail.com>
2023-09-11 15:50:09 -0400

    debugging

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,6 +3,10 @@
 
 
 def lookup(obj):
-	"""this module the list of available attributes and methods of an object"""
-	
-    return dir(obj)
+    """this module the list of available attributes and methods of an object"""
+    new_list = []
+    for i in dir(obj):
+        if i == '__init_subclass__':
+            continue
+        new_list.append(i)
+    return new_list
