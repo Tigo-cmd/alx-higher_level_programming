92f5387f7bb69a907743c67379b18c03661c0b53
Tigo-cmd <ja16162040@gmail.com>
2023-09-11 17:12:17 -0400

    returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False.

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-def inherits_from(obj, a_class):
+"""implement the isinstance method"""
+
+
+def is_kind_of_class(obj, a_class):
+    """it is applied based on 3-is_kind_of_class"""
+    return isinstance(obj, a_class)
+
