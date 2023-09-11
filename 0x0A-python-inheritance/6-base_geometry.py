bc49c40d354ee9d8e8e6bc28c61ce4b6fd80aadb
Tigo-cmd <ja16162040@gmail.com>
2023-09-11 17:59:13 -0400

    raises an exception if area() is not implemented

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,6 +5,5 @@
 class BaseGeometry:
     """implements the base geometry"""
     def area(self):
-    """implementes the area of the base_geometry and raises an exception if not implemented"""
+    """raises an exception if not implemented"""
         raise Exception("area() is not implemented")
-    pass
