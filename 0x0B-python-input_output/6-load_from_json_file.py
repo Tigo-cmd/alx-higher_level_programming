cfb12d400b65090e7ea568a28e4e0a20c92ffc1c
Tigo-cmd <ja16162040@gmail.com>
2023-09-13 13:06:16 -0400

    . Create object from a JSON file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+"""this module creates an Object from a “JSON file”:"""
+
+
+import json
+
+
 def load_from_json_file(filename):
+    """this function  creates an Object from a “JSON file”:
+       Args:
+           filename: text file to be converted from to "JSON file"
+       Return:
+          nothing
+    """
+    with open(filename, 'r') as file:
+        convert = json.loads(file.read())
+    return convert
