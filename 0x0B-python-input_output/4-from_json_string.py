4671929a947668407a3f7f084c12562b1e06e359
Tigo-cmd <ja16162040@gmail.com>
2023-09-13 12:35:12 -0400

    From JSON string to Object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+"""this module returns an object (Python data structure)
+represented by a JSON string
+"""
+
+
+import json
+
+
 def from_json_string(my_str):
+    """function  returns an object (Python data structure)
+       represented by a JSON string
+       Args:
+           my_str: string to be converted to PDS
+       Return:
+          (Python data structure) represented by a JSON string
+    """
+    return json.loads(my_str)
