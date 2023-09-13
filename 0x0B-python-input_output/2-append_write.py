d1a8d73631bc69c46ee60a9a06bbaac3c79254f7
Tigo-cmd <ja16162040@gmail.com>
2023-09-13 12:07:37 -0400

    Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-Append to a file
+"""this module appends  a string to a text file (UTF8)
+   and returns the number of characters added:
+"""
+
+
+def append_write(filename="", text=""):
+    """function that appends a string to a text file (UTF8)
+       and returns the number of characters added
+       Args:
+           filename: file to be appended and written to
+           text: string to be appended to filename
+       Return:
+           no of characters added
+    """
+    with open(filename, 'a', encoding="utf-8") as file:
+        main = file.write(text)
+    return main
