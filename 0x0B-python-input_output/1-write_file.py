afd80927769c950f4a5e17f09884f935c2e7ba25
Tigo-cmd <ja16162040@gmail.com>
2023-09-13 06:13:56 -0400

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-Write to a file
+"""this module writes a string to a text file (UTF8)
+   and returns the number of characters written:
+"""
+
+
+def write_file(filename="", text=""):
+    """function that writes a string to a text file (UTF8)
+       and returns the number of characters written
+       Args:
+           filename: file to be read and writing to
+           text: string to be writing to filename
+    """
+    with open(filename, encoding="utf-8") as file:
+        file.write(text)
