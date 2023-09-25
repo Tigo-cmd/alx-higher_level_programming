334aeddff303b38f0407af51f3659cfb31ce3c8c
Tigo-cmd <ja16162040@gmail.com>
2023-09-25 19:36:24 -0400

    square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -88,5 +88,5 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """returns a new Rectangle instance with width == height == size"""
-        return cls(size, size)
+        return Rectangle
 
