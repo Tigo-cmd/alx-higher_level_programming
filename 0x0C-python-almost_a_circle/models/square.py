78baceeca72b7322beda362e549ad76a8ca6c72e
Tigo-cmd <ja16162040@gmail.com>
2023-09-28 03:18:21 -0400

    Almost a Circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,11 +27,6 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        """set the property for the size of the square"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
