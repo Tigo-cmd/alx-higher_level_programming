c74d6f6354407ff8024041c9567bf8fd2f81d5b6
Tigo-cmd <ja16162040@gmail.com>
2023-09-28 03:24:50 -0400

    almost a circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,13 +17,14 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """overloading method which returns 
+        """overloading method which returns
         [Square] (<id>) <x>/<y> - <size> - width or height
         """
         ret = f"[{self.__class__.__name__}] "
         ret += f"({self.id}) {self.__x}/{self.__y}"
         ret += f" - {self.size}"
         return ret
+        
     @property
     def size(self):
         """return the size of the square"""
