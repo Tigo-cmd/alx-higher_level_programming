93bc3bcd2d338007ebde4171ce4c53db4adb88dd
Tigo-cmd <ja16162040@gmail.com>
2023-10-01 16:27:18 -0400

    8-class_to_json.py:10:80: E501 line too long (99 > 79 characters)

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -7,7 +7,8 @@ JSON serialization of an object:
 
 def class_to_json(obj):
     """
-    All attributes of the obj Class are serializable: list, dictionary, string, integer and boolean
+    All attributes of the obj Class are serializable:
+     list, dictionary, string, integer and boolean
     :param obj:  instance of a Class
     :return: the dictionary description with simple data structure
     (list, dictionary, string, integer and boolean)
