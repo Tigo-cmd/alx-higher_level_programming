66cf499eb25a43f57c20d9da9ad4fa425201c780
Tigo-cmd <ja16162040@gmail.com>
2023-10-01 16:26:27 -0400

    class_to_json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+"""  returns the dictionary description with simple data structure
+(list, dictionary, string, integer and boolean) for
+JSON serialization of an object:
+"""
+
+
 def class_to_json(obj):
+    """
+    All attributes of the obj Class are serializable: list, dictionary, string, integer and boolean
+    :param obj:  instance of a Class
+    :return: the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object:
+
+    """
+    return obj.__dict__
