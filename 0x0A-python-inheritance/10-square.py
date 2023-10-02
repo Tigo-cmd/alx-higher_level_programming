a1d902b3d41abeaef9bd14c5c3f286ba74bbab7f
Tigo-cmd <ja16162040@gmail.com>
2023-10-02 09:34:52 -0400

    10. Square #1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """class Square that inherits from Rectangle"""
-
 Rectangle = __import__("9.rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """
-    class Square that inherits from Rectangle
-    """
+    """class Square that inherits from Rectangle"""
 
     def __init__(self, size):
+    """init a square"""
         super().__init__(size, size)
