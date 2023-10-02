ccd85165c27756a391eba0c0fa8bad36289131fa
Tigo-cmd <ja16162040@gmail.com>
2023-10-02 17:34:26 -0400

    11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Defines a Square class that inherits a Rectangle"""
+Square = __import__("9.rectangle").Rectangle
+
+
+class Square(Square):
+    """implements the square class"""
+
+    def __init__(self, size):
+        """initializes a square
+	       
+	Args:
+	size: size of the square
+	"""
+        super().__init__(size, size)
+
