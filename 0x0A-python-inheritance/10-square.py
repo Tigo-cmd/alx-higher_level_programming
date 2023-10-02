9691cdcf33df51fc55337d2381bc729d762a3860
Tigo-cmd <ja16162040@gmail.com>
2023-10-02 16:59:59 -0400

    commit

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""class Square that inherits from Rectangle"""
-Rectangle = __import__("9.rectangle").Rectangle
+"""Defines a child square class: inherits from Rectangle"""
+Square1 = __import__("9-rectangle").Rectangle
 
 
-class Square(Rectangle):
-    """class Square that inherits from Rectangle"""
+class Square(Square1):
+    """Represents a square"""
 
     def __init__(self, size):
-    """init a square"""
+        """Initialisez a square
+
+        Args:
+        size (int): size of square
+        """
         super().__init__(size, size)
