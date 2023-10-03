c94a3ffd91360d9ea9e50a7a0af3e96347969b9b
Tigo-cmd <ja16162040@gmail.com>
2023-10-03 00:58:58 -0400

    commit

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Defines a Square class that inherits a Rectangle"""
-Square = __import__("9.rectangle").Rectangle
+"""Defines a child square class: inherits from Rectangle"""
+Square1 = __import__("9-rectangle").Rectangle
 
 
-class Square(Square):
-    """implements the square class"""
+class Square(Square1):
+    """Represents a square"""
 
     def __init__(self, size):
-        """initializes a square
-	       
-	Args:
-	size: size of the square
-	"""
+        """Initialisez a square
+
+        Args:
+        size (int): size of square
+        """
         super().__init__(size, size)
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Defines a square class that inherits from Rectangle"""
-Square = __import__("9-rectangle").Rectangle
+"""Defines a child square class: inherits from Rectangle"""
+Square1 = __import__("9-rectangle").Rectangle
 
 
-class Square(Square):
-    """implements  a square"""
+class Square(Square1):
+    """Represents a square"""
 
     def __init__(self, size):
         """Initialisez a square
 
         Args:
-        size: size of square
+        size (int): size of square
         """
         super().__init__(size, size)
