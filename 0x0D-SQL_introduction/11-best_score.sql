670f36a012e5dbc43792fd70d137ff226c7dd864
Tigo-cmd <ja16162040@gmail.com>
2023-10-19 21:08:16 +0100

    No cheating

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,2 @@
--- a script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, name
-FROM second_table
-WHERE score >= 10
-ORDER BY score DESC;
+-- script that shows list of all records of score >= 10 in the table of a database
+SELECT second_table
