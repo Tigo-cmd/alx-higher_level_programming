db699e572c0952e9524eb08c8f58b7e32f64c46a
Tigo-cmd <ja16162040@gmail.com>
2024-03-13 05:47:08 +0100

    This program will assign a random signed number to the variable number each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable number.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+check_negative = number < 0
+print(f"Last digit of {number} is ", end="")
+if check_negative:
+    number = number * -1
+out = number % 10
+if check_negative:
+    out = out * -1
+if out > 5:
+    print("{} and is greater than 5".format(out))
+elif out == 0:
+    print(f"{out} and is 0")
+elif out < 6 and not 0:
+    print(f"{out} and is less than 6 and not 0")
