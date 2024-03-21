7d5fc21f8398468568f08698847dbfd68bc95b1a
Tigo-cmd <ja16162040@gmail.com>
2024-03-21 04:17:55 +0100

    fixed contitional bugs

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
+if __name__ == "__main__":
     import sys
     n = len(sys.argv) - 1
     if n == 0:
         print("0 arguments.")
     else:
         print("{} arguments:".format(n))
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
