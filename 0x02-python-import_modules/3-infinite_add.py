41a1b217e284a23fbf051661cc4e335c5e8f8905
Tigo-cmd <ja16162040@gmail.com>
2024-03-21 04:55:18 +0100

    infinite add arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) == 1:
+        print("0")
+    else:
+        man = 0
+        for i in range(1, len(sys.argv)):
+            man += int(sys.argv[i])
+        print(man)
+
