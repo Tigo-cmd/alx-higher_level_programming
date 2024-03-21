9d2aa853bd4cc451965895cc8a48cbad27d8d295
Tigo-cmd <ja16162040@gmail.com>
2024-03-21 05:40:01 +0100

    variable load

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+    words = dir(hidden_4)
+    final = []
+    for word in words:
+        if word.startswith("__"):
+            continue
+        final.append(word)
+    final.sort()
+    for i in final:
+        print(i)
