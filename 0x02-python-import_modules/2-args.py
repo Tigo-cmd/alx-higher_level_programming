dcb3ff4fb12b5a2b8a0f1db67bdd04eb69df769d
Tigo-cmd <ja16162040@gmail.com>
2024-03-21 04:11:00 +0100

    a program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv) - 1
-if n == 0:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(n))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ = "__main__":
+    import sys
+    n = len(sys.argv) - 1
+    if n == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(n))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
