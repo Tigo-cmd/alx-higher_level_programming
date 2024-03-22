1bcfc7aaf9dc8783b7fa5d656224d205c688ba9a
Tigo-cmd <ja16162040@gmail.com>
2024-03-22 15:09:35 +0100

    a function that adds two integers and returns the result.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print(f"{i}", end="")
+        print(f"{j}, ", end="")
+print(89)
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
+    new_list = []
+    for word in str:
+        print(chr(ord(word) - 32), end="" if 92 <= ord(word) <= 122 else word)
+    print()
