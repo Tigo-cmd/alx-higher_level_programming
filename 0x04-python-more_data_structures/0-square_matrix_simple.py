7c1d6368547c3583453986b8939ad36828b417b8
Tigo-cmd <ja16162040@gmail.com>
2024-03-30 00:52:29 +0100

    fixed some files

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,8 @@
 def square_matrix_simple(matrix=[]):
     new = []
     for i in matrix:
-        new.append([pow(i[j], 2) for j in range(3)])
+        new2 = []
+        for j in i:
+            new2.append(j ** 2)
+        new.append(new2)
     return new
