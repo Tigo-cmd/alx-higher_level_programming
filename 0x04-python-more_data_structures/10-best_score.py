ca6228c4f57e3695aa5d05c59789cd326edbf652
Tigo-cmd <ja16162040@gmail.com>
2024-03-31 00:07:03 +0100

    a function that returns a key with the biggest integer value.

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    j = 0
+    upper = 0
+    if not a_dictionary:
+        return None
+    else:
+        num = list(a_dictionary.values())
+        init = list(a_dictionary.keys())
+        upper = max(num)
+        for i in range(len(num)):
+            if num[i] == upper:
+                j = i
+        return init[j]
