2d4ece12ecbaeda897edad21fab4700a14de4f91
EmmanuelTigo <ja16162040@gmail.com>
2024-04-02 13:56:07 -0700

    Create 4-list_division.py

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+new - []
+try:
+  result = my_list[i] / my_list_2[i]
+except (TypeError, IndexEror)
+  print("wrong type")
+  result = 0
+except(ZeroDivisionError)
+  print("wrong type")
+  result = 0
+finally:
+  new.append(result)
+return result
