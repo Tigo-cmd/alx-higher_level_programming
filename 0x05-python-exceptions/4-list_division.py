079048f7372ff4e3473afef9b770ef58e2932526
Tigo-cmd <ja16162040@gmail.com>
2024-04-03 20:18:52 +0100

    fixed some files

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,18 +1,25 @@
-#!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    new = []
-    for i in range(list_length):
-        try:
-            result = my_list_1[i] / my_list_2[i]
-        except TypeError:
-            print("wrong type")
-            result = 0
-        except ZeroDivisionError:
-            print("division by 0")
-            result = 0
-        except IndexError:
-            print("out of range")
-            result = 0
-        finally:
-            new.append(result)
-    return result
+    result = []
+    try:
+        for i in range(list_length):
+            try:
+                num1 = my_list_1[i] if i < len(my_list_1) else 0
+                num2 = my_list_2[i] if i < len(my_list_2) else 0
+                if isinstance(num1, (int, float)) and isinstance(num2, (int, float)):
+                    try:
+                        division_result = num1 / num2
+                        result.append(division_result)
+                    except ZeroDivisionError:
+                        print("division by 0")
+                        result.append(0)
+                else:
+                    print("wrong type")
+                    result.append(0)
+            except IndexError:
+                print("out of range")
+                result.append(0)
+    except:
+        print("An error occurred")
+    finally:
+        return result
+
