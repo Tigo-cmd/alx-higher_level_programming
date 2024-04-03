af8ce826ee977564009796bb3e1012a0e4f00781
Tigo-cmd <ja16162040@gmail.com>
2024-04-03 20:16:28 +0100

    fixed some files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-   """
- Prints x elements of a list, handling potential errors.
-
- Args:
-     my_list: The list to print elements from (default empty list).
-     x: The number of elements to print (default 0).
-
- Returns:
-     The number of elements actually printed.
- """
-  k = 0
-  for i in range(x):
-    try:
-      print(my_list[i], end=" ")
-      k += 1
-    except IndexError:
-      pass
-  print()
-  return k
-          
+    k = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            k += 1
+        except IndexError:
+            pass
+    print()
+    return k
