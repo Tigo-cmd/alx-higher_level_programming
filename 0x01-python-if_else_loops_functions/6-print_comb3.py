fe2a925e8d7deac51a30d66df704fc86393374bb
Tigo-cmd <ja16162040@gmail.com>
2024-04-05 23:24:27 +0100

    a function that adds two integers and returns the result.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 8):
     for j in range(i + 1, 10):
-        print(f"{i}", end="")
-        print(f"{j}, ", end="")
+        print("{}".format(i), end="")
+        print("{}, ".format(j), end="")
 print(89)
