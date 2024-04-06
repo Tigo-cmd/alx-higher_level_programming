4fc087c48941b3c0934548f1f7535954cd907efc
Tigo-cmd <ja16162040@gmail.com>
2024-04-06 11:09:54 +0100

    a class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """ an empty class Square that defines a square"""
-    def __int__(self, size):
+    def __init__(self, size):
         """ constructor for Square method
             Private instance attribute: size
             Instantiation with size (no type/value verification)
