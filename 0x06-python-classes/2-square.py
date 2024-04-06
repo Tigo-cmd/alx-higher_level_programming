e3b647a3fc66a27f0965cc746416fff551cc86ec
Tigo-cmd <ja16162040@gmail.com>
2024-04-06 11:30:12 +0100

    a class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,9 +10,9 @@ class Square:
             Args:
                 size: private instance attribute
             Instantiation with optional size: def __init__(self, size=0):
-            size must be an integer, otherwise raise 
+            size must be an integer, otherwise raise
             a TypeError exception with the message size must be an integer
-            if size is less than 0, raise a ValueError 
+            if size is less than 0, raise a ValueError
             exception with the message size must be >= 0
         """
         if not isinstance(size, int):
