1bb3d355dede501213d73102e33fde81d9fa4e61
Tigo-cmd <ja16162040@gmail.com>
2024-04-06 11:20:02 +0100

    a class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Square Class module"""
+
+
+class Square:
+    """ an empty class Square that defines a square"""
+    def __init__(self, size=0):
+        """ constructor for Square method
+            Private instance attribute: size
+            Args:
+                size: private instance attribute
+            Instantiation with optional size: def __init__(self, size=0):
+            size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
+            if size is less than 0, raise a ValueError exception with the message size must be >= 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
