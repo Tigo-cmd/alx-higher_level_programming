84e7b8228e2526b4e94e92d3d1c4dacd9df72968
Tigo-cmd <ja16162040@gmail.com>
2024-04-15 22:54:40 +0100

    Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-4-inherits_from.py
+"""defines a inherited class checking module"""
+
+
+def inherits_from(obj, a_class):
+    """a function that returns True if the object is an instance of
+     a class that inherited (directly or indirectly) from the specified class ; otherwise False.
+    :param obj: object to be checked
+    :param a_class:the class
+    :return: True if meets requirements else False
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
