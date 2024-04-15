2fd845b03a5a91d896ef5995de02d8cb15ad48b9
Tigo-cmd <ja16162040@gmail.com>
2024-04-15 23:04:26 +0100

    6. Improve Geometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""defines a base geometry class"""
+
+
+class BaseGeometry:
+    """a base Geometry class"""
+
+    def __init__(self):
+        """initializing function
+        """
+
+    def area(self):
+        """returns the area of the geometry"""
+        raise Exception("area() is not implemented")
