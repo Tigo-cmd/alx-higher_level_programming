0faaedb2efbc69035a38fb90b7b94d023ee641a7
Tigo-cmd <ja16162040@gmail.com>
2024-04-15 23:31:00 +0100

    8. Rectangle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""defines a base geometry class depending on 6-base_geometry"""
+
+
+class BaseGeometry:
+    """a base Geometry class"""
+
+    def __init__(self):
+        """initializing function at first call
+        """
+
+    def area(self):
+        """returns the area of the geometry"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates an integer"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""defines a Rectangle class module"""
+
+
+class Rectangle:
+    """a rectangle class that inherits from BaseGeometry class
+    """
+
+    def __init__(self, width, height):
+        """initializing function at first call
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+        """
+        super().__init__()
+        self.__width = width
+        self.__height = height
