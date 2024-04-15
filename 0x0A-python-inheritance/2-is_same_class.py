aea2eab082d27e26ebc1250f54057cf1021903da
Tigo-cmd <ja16162040@gmail.com>
2024-04-15 22:37:59 +0100

    a function that returns True if the object is exactly an instance of the specified class ; otherwise False.

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+"""defines an instance checking function"""
+
+
 def is_same_class(obj, a_class):
+    """
+    a function that returns True if the object is exactly
+    an instance of the specified class ; otherwise False.
+    :param obj: object to be checked
+    :param a_class: a class
+    :return: True if object is an instance else false
+    """
+    if type(obj) != a_class:
+        return False
+    return True
