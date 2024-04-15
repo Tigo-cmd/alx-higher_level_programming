0b2d1099b98e557546bb03ccb847473689af9a8f
Tigo-cmd <ja16162040@gmail.com>
2024-04-15 23:46:02 +0100

    knkl

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""defines a Square class module"""
+
+
+class Square(Rectangle):
+    """a rectangle class that inherits from Rectangle class"""
+
+    def __init__(self, size):
+        """initializing function at first call
+        Args:
+            size: width of the square
+        """
+        super().__init__()
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
