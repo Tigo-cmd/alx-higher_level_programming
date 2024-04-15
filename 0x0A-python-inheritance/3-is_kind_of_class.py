5120335dde6ffbc513641019928527cdb4c55063
Tigo-cmd <ja16162040@gmail.com>
2024-04-15 22:44:28 +0100

    Same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+"""an instance checking function module"""
+
+
 def is_kind_of_class(obj, a_class):
+    """
+    checks if an object is an instance of, or if the objects is
+    an instance of a class it inherited from, the specified class
+    else, false
+    :param obj: object to be checked
+    :param a_class: the class tobe checked form
+    :return: True if meets requirements else false
+    """
+    if not isinstance(obj, a_class):
+        return False
+    return True
