e392cade1ee2ffe33f7f81039674c2d00024bc0e
Tigo-cmd <ja16162040@gmail.com>
2024-04-16 14:50:00 +0100

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,7 +8,6 @@ def read_file(filename=""):
     :param filename: file to be read
     :return: nothing
     """
-    
-    with open(filename, encoding='UTF8') as file:
+    with open(filename, encoding='utf-8') as file:
         """reads the files and automatically closes it"""
         file.read()
