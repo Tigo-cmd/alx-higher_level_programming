524f752522c8ce96c336934a3f153e9dc09e4076
Tigo-cmd <ja16162040@gmail.com>
2024-04-16 18:06:09 +0100

    to_json

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""Defines a student class module of functionality"""
+"""Defines a student class module"""
 import json
 
 
 class Student:
     """a class Student that defines a student initials"""
     def __init__(self, first_name, last_name, age):
-        """initializing function at first call
-            first_name: students first name
-            last_name: students last name
-            age: students age
+        """initializing function at first callS
+           first_name: students first name
+           last_name: students last name
+           age: students age
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
+        
     def to_json(self):
         """retrieves a dictionary representation of a Student instance"""
         return self.__dict__
