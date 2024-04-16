f755e0d9f07c756e379d8271b0bf7c1802100e13
Tigo-cmd <ja16162040@gmail.com>
2024-04-16 15:18:15 +0100

    3. To JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""json manipulation function module"""
+import json
+
+
 def to_json_string(my_obj):
+    """
+    handles JSON representation of an object(string)
+    :param my_obj: str to be converted to json
+    :return: converted object as JSON
+    """
+    return json.dumps(my_obj)
