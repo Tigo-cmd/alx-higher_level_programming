cb7e3c699a46833b5dd1d370a90a4bee7ad6b605
Tigo-cmd <ja16162040@gmail.com>
2024-04-16 15:14:15 +0100

    . Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+"""defines file appending function module"""
+
+
 def append_write(filename="", text=""):
+    """
+    appends a string at the end of a text file
+    :param filename:  text file to be appended to
+    :param text: text to be appended
+    :return: number of characters added
+    """
+    with open(filename, "a", encoding="utf-8" ) as f:
+        return f.write(text)
