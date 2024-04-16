a4f8403a5b8e6849a1aaf70ee3562b8fc260c19f
Tigo-cmd <ja16162040@gmail.com>
2024-04-16 13:27:44 +0100

    can i? ooo yes ic can

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-add_attribute(class, name, value):
+"""this defines an attribute adder function"""
+
+
+def add_attribute(a_class, name, value):
+    """adds attribute to a class object
+       Args:
+           a_class = class object
+           name = attribute of the class to be set
+           value = param of the name attribute
+    """
+    if hasattr(a_class, '__dict__'):
+        setattr(a_class, name, value)
+    else:
+        raise TypeError("can't add new attributes")
