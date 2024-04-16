7159ff54d322644f6d047368bde967ab267b76a2
Tigo-cmd <ja16162040@gmail.com>
2024-04-16 11:44:55 +0100

    inheritance from subclass

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,7 +3,7 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectangle:
+class Rectangle(BaseGeometry):
     """a rectangle class that inherits from BaseGeometry class"""
 
     def __init__(self, width, height):
