fbf606f9c588df3365da0c114aed453bbd263dfd
Tigo-cmd <ja16162040@gmail.com>
2024-04-16 15:23:53 +0100

    From JSON string to Object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""json manipulation fxn module"""
+import json
 
+
+def from_json_string(my_str):
+    """
+    handles an object (Python data structure) represented by a JSON string:
+    :param my_str: jSON string
+    :return: an object (Python data structure) represented by a JSON string:
+    """
+    return json.loads(my_str)
