d1db71ae82026d76e05721790dd74acc8908a180
Tigo-cmd <ja16162040@gmail.com>
2024-04-16 11:52:23 +0100

    implemented the inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """defines a Square class module for functions"""
+Rectangle = __import__(9-rectangle.py).Rectangle
 
 
 class Square(Rectangle):
@@ -13,6 +14,7 @@ class Square(Rectangle):
             size: width of the square
         """
         super().__init__()
+        self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
