cd333eda66de089ab60f0842719ac461420d1bb6
Tigo-cmd <ja16162040@gmail.com>
2024-04-16 15:35:44 +0100

    5. Save Object to a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes an object to atext file using a Json rep"""
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "w") as f:
         return json.dumps(my_obj, f)
