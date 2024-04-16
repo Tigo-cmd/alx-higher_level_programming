ff2f0407d38f4ff25750cfe69b4dc416153d5b07
Tigo-cmd <ja16162040@gmail.com>
2024-04-16 21:49:26 +0100

    fixing docttest

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""Defines a student class module function"""
-import json
+"""class Student that defines a student"""
 
 
 class Student:
-    """a class Student that defines a student """
+    """
+     class Student that defines a student by:
+
+    Public instance attributes:
+        first_name
+        last_name
+        age
+    """
     def __init__(self, first_name, last_name, age):
-        """initializing function at first callS
-           first_name: students first name
-           last_name: students last name
-           age: students age
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self):
-        """retrieves a dictionary representation of a Student instance"""
+        """
+        retrieves a dictionary representation
+         of a Student instance (same as 8-class_to_json.py)
+        """
         return self.__dict__
