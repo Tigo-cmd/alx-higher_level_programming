1d69287ed4f4770218e28a054af7f1f3f9ce8d48
Tigo-cmd <ja16162040@gmail.com>
2024-04-16 16:41:39 +0100

    . Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""another manipulative function module"""
+import sys
+
+if __name__ == "__main__":
+    save_to_json = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json = __import__('6-load_from_json_file').load_from_json_file
+    file = "add_item.json"
+
+    try:
+        content = load_from_json(file)
+    except FileNotFoundError:
+        content = []
+
+    content.extend(sys.argv[1:])
+    save_to_json(content, file)
