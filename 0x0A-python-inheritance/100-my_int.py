ff8a247a56ee2d4b71d192ec1d83be0e9fd22620
Tigo-cmd <ja16162040@gmail.com>
2024-04-16 12:58:01 +0100

    rebel class

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""defines a rebel class module thats rebels int class"""
+
+
+class MyInt(int):
+    """this class inherits from the int class it's a rebel
+     	MyInt is a rebel. MyInt has == and != operators inverted
+    """
+
+    def __init__(self, num):
+        """initialization rebel function"""
+        super().__init__()
+        self.__num = num
+
+    def __eq__(self, other):
+        """implements the inverted function"""
+        if self.__num == other:
+            return False
+        return True
+
+    def __ne__(self, other):
+        """implements the rebel of __eq__()"""
+        if self.__num != other:
+            return False
+        return True
