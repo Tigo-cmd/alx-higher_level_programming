fdb550558686696c316286a589b4c47bc388b94a
Tigo-cmd <ja16162040@gmail.com>
2024-04-16 17:37:02 +0100

    9. Student to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""defines a function for json manipulation module"""
+
+
 def class_to_json(obj):
+    """
+    returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON serialization of an object
+    """
+    return obj.__dict__
