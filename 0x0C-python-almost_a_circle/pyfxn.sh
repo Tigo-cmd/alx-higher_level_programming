5aff141e457438a828faed7259e5446ff452a40e
Tigo-cmd <ja16162040@gmail.com>
2024-04-18 10:39:02 +0100

    alx-higher_level_programming

---
+++ 0x0C-python-almost_a_circle/pyfxn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "file >"
+read file
+touch $file
+echo "#!/usr/bin/python3" > $file
+echo "Prototype >"
+read proto 
+echo $proto >> $file
+chmod 764 $file
+git add $file
+bash dirgit.sh
+gedit $file
